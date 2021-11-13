c402239749818982a6fd288efbd8452cdd299f2a
josephgan <josephgan1988@gmail.com>
2021-11-13 19:31:05 +0800

    add webapp

--- WebAPP/api.py
+++ WebAPP/api.py
@@ -6,11 +6,12 @@ about whether Flask is working. A short check is run at the bottom of the file.
 """
 
 import pickle
+import dill
 import numpy as np
 import pandas as pd
 import re
 
-
+#--------------------prediction------------------------
 # Open relevant models
 with open("static\models\XGBoost_Model.pkl", "rb") as to_read:
     model = pickle.load(to_read)
@@ -19,7 +20,7 @@ with open("static\models\District_Transformer.pkl", "rb") as to_read:
      transformer = pickle.load(to_read)
 
 #read main file        
-main_df = pd.read_csv('..\Data\scraped_df.csv')
+main_df = pd.read_csv('..\Data\main_df.csv')
 main_df.loc[main_df.district == 'Changi Airport / Changi Village (D17)', 'mrt_name'] = 'Tampines East Mrt'
 main_df.dropna(subset=['mrt_name'], inplace=True)
 
@@ -40,8 +41,12 @@ def convert(x_input):
     mapping = transformer.encoder_dict_['district_number']
     
     #regex to extract district number
+    global district_number_before_transform
+    
     district_number_before_transform = int(re.findall(f'D([\d]+)',  x_input['district'])[0])
     district_number = mapping[district_number_before_transform]
+    
+    
     
     Dhoby_Ghaut_MRT = 0
     Holland_Village_MRT = 0
@@ -92,16 +97,107 @@ district_list
 # for import
 district_mrt
 
+
+
+#----------------------------recommendation-----------------------
+with open("static\models\Kneighbour.pkl", "rb") as to_load:
+    neigh = pickle.load(to_load)
+
+with open("static\database\df_recommender.pkl", "rb") as to_load:
+    df_recommender = pickle.load(to_load)   
+
+with open("static\models\onehotencoder_Transformer.pkl", "rb") as to_load:
+    ct = pickle.load(to_load)
+
+with open("static\models\FeatureImportanceScale.joblib", "rb") as to_load:
+    feature_importance = dill.load(to_load)
+       
+def customtransformation(X):
+    """
+    Function to combine
+    
+    1.OneHotEncoder
+    2.MixMaxScaler
+    3.FeatureWeighting
+    """
+    #combine feature_name
+    #feature of OHE
+    feature1 = ct.named_transformers_.onehotencoder.get_feature_names().tolist()
+    
+    #remaining feature
+    feature2 = ['built_year', 'walking_time_to_mrt', 'sqft', 'price_month', 'district', 'pool', 'gym']
+
+    #combine 
+    all_feature = feature1 + feature2
+    
+    X = pd.DataFrame.from_dict(X, orient='index').T
+    
+    #rearrange columns as per fit
+    rearrange = ['district', 'mrt', 'built_year', 'walking_time_to_mrt', 'sqft', 'pool', 'gym', 'price_month']
+
+    X = X[rearrange]
+    
+    # #casting numerical value
+
+    for col in X:
+        X[col] = pd.to_numeric(X[col], errors='ignore')
+    #transform to Dataframe for Feature Importance    
+    X_tr = pd.DataFrame(ct.transform(X).toarray(), columns=all_feature)
+    X_tr = feature_importance.transform(X_tr)
+    
+    return X_tr
+
+def recommendlisting(user_input, prediction):
+
+    district_number_before_transform = int(re.findall(f'D([\d]+)',  user_input['district'])[0])
+
+    user_input['district'] = district_number_before_transform
+    user_input['price_month'] = prediction
+       
+    X = customtransformation(user_input)
+    
+    index = neigh.kneighbors(X,  return_distance=False)
+    selected_index = np.reshape(index, -1)
+    
+    sim = df_recommender.iloc[selected_index]
+    
+    price_month = sim.price_month.tolist()
+    district = sim.district.tolist()
+    detailed_address = sim.detailed_address.tolist()
+    bedrooms = sim.bedrooms.tolist()
+    bathrooms = sim.bathrooms.tolist()
+    sqft = sim.sqft.tolist()
+    built_year = sim.built_year.tolist()
+    mrt = sim.mrt.tolist()
+    walking_time_to_mrt = sim.walking_time_to_mrt.tolist()
+    link = sim.link.tolist()
+    picture_url	= sim.picture_url.tolist()
+    
+    return (price_month, 
+        district, 
+        detailed_address, 
+        bedrooms, 
+        bathrooms, 
+        sqft, 
+        built_year, 
+        mrt, 
+        walking_time_to_mrt, 
+        link, 
+        picture_url)
+
 if __name__ == '__main__':
     user_input = {
-        'district' : 'Pasir Ris / Tampines (D18)',
+        'district' : 'Beach Road / Bugis / Rochor (D7)',
         'mrt' : 'Tampines MRT',
-        'built_year' : 1980,
-        'walking_time_to_mrt' : 22,
-        'sqft' : 123,
-        'pool' : 1,
-        'gym': 1,
+        'built_year' : '1980',
+        'walking_time_to_mrt' : '22',
+        'sqft' : '123',
+        'pool' : '1',
+        'gym': '1'
     }
 
-    result = make_prediction(user_input)
-    print(result)
+    prediction = make_prediction(user_input)
+    print(prediction)
+    listing = recommendlisting(user_input, prediction)
+
+    
--- WebAPP/app.py
+++ WebAPP/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from api import district_list, district_mrt, mrt_list, year_min, year_max, make_prediction
+from api import district_list, district_mrt, mrt_list, year_min, year_max, make_prediction, recommendlisting
 import numpy as np
 import pandas as pd
 import pickle
@@ -25,12 +25,37 @@ def form():
 @app.route('/results')
 def predict():
     # use flask's render_template function to display an html page
-    user_input = request.args
+    user_input = request.args.to_dict()
     
     prediction = make_prediction(user_input)
-    
-    return render_template('results.html', prediction = prediction)
 
+    recommendation = recommendlisting(user_input, prediction)
+
+    price_month = recommendation[0]
+    district = recommendation[1]
+    detailed_address = recommendation[2]
+    bedrooms = recommendation[3]
+    bathrooms = recommendation[4]
+    sqft = recommendation[5]
+    built_year = recommendation[6]
+    mrt = recommendation[7]
+    walking_time_to_mrt = recommendation[8]
+    link = recommendation[9]
+    picture = recommendation[10]
+    
+    return render_template('results.html', 
+                            prediction = prediction,
+                            price_month = price_month,
+                            district = district,
+                            detailed_address = detailed_address,
+                            bedrooms = bedrooms,
+                            bathrooms = bathrooms,
+                            sqft = sqft,
+                            built_year = built_year,
+                            mrt = mrt,
+                            walking_time_to_mrt = walking_time_to_mrt,
+                            link = link,
+                            picture = picture)
 
 if __name__ == "__main__":
     app.run(debug=True)
